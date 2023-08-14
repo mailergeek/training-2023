fdf47450f9ff5747bc9f734990cbdf0b1cb2b615
shamnas10 <shamnasmuhammed78@gmail.com>
2023-08-14 18:35:27 +0530

    updated

--- shamnas-mysql.sh
+++ shamnas-mysql.sh
@@ -1,14 +1,26 @@
 start=$(date +%s%3N)
-DB_USER="root"
-DB_PASSWORD="newpassword"
 DB_NAME="mydatabase"
 CSV_FILE="/home/shamnas/Documents/employee_details.csv"
+TABLE_NAME="employee" 
 
-while IFS="," read -r id username email password firstname lastname jobtitle company city state postalcode; do
 
-mysql --defaults-file=~/.my.cnf -D "$DB_NAME" -e "INSERT INTO employee (id,username,email,password,firstname,lastname,jobtitle,company,city,state,postalcode) VALUES ('$id','$username','$email','$password','$firstname','$lastname','$jobtitle','$company','$city','$state','$postalcode');"
+my_value=""
 
+while IFS="," read -r id username email password firstname lastname jobtitle company city state postalcode; 
+do
+ my_value+="('$id','$username','$email','$password','$firstname','$lastname','$jobtitle','$company','$city','$state','$postalcode'),"
 done < "$CSV_FILE"
+
+# Remove the trailing comma from my_values
+my_value="${my_value%,}"
+
+# my_value=$(echo "$my_value" | sed 's/,$//')
+
+# Connect to the database and insert the values
+mysql --defaults-file=~/.my.cnf -D "$DB_NAME" -e "
+INSERT INTO $TABLE_NAME(id,username,email,password,firstname,lastname,jobtitle,company,city,state,postalcode)
+VALUES  $my_value;
+"
 end=$(date +%s%3N)
 execution_time=$((end - start))
 echo "Execution time: $execution_time ms"
