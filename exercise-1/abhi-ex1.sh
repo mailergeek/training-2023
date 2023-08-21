b831e3b72c2ef63700d0a62275a203c0312e0641
Abhinandh AV <abhinandh.avh@gmail.com>
2023-08-21 17:29:59 +0530

    Code updated able to insert 10 lakh data in 50 minutes

--- exercise-1/abhi-ex1.sh
+++ exercise-1/abhi-ex1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-start_time=$(date +%s%3N) 
+
+start_time=$(date +%s%3N)
+batch_size=10
+
 generate_data() {
     data=""
     nested_array=()
@@ -29,52 +32,61 @@ generate_data() {
             nested_array=(1 3 4 7)
             ;;
         *)
-           
             echo "Unknown key: $activity"
             ;;
     esac
     for ((i=0; i<${#nested_array[@]}; i++)); do
         array_value="${nested_array[$i]}"
         activity_date=$(date -d "$activity_date + $((i+1)) days" +%Y-%m-%d)
-        activity_string="($id, $campaign_ID, $array_value, '$activity_date')"
+        activity_string="(@parent_id, $campaign_ID, $array_value, '$activity_date')"
         if [[ $i -ne 0 ]]; then
-            data+=",$activity_string"
+            data+=", $activity_string"
         else
-            data+="$activity_string"
+            data="$activity_string"
         fi
     done
-
-
     echo "$data"
 }
 
 
-id='@parent_id'
-
-
-DB_NAME="DB"
-count=100
+count=1000000
+j=0  
 
 for ((i=1; i<=$count; i++)); do
-  name="Name$i"
-  email="name$i@example.com"
-  dob="01/01/2000"  
-  country="Country$i"
-  city="City$i"
-  json=("{\"dob\":\"$dob\",\"country\":\"$country\",\"city\":\"$city\"}")
-  result=$(generate_data)
+   
+    if [[ $j -lt 1000 ]]; then  
+        name="Name$i"
+        email="name$i@example.com"
+        dob="01/01/2000"
+        country="Country$i"
+        city="City$i"
+        json="{\"dob\":\"$dob\",\"country\":\"$country\",\"city\":\"$city\"}"
+        result=$(generate_data)
 
-    mysql --defaults-file=~/.my.cnf $DB_NAME << EOF
+        batch_data+="INSERT INTO contacts (name, email) VALUES ('$name', '$email');"
+        batch_data+="SET @parent_id = LAST_INSERT_ID();"
+        batch_data+="INSERT INTO contacts_details (contacts_ID, json) VALUES (@parent_id, '$json');"
+        batch_data+="INSERT INTO contacts_activity (contacts_ID, campaign_ID, activity_type, activity_date) VALUES $result;"
+        j=$((j+1))  
+    else
+        mysql --defaults-file=~/.my.cnf -D DB <<EOF
 START TRANSACTION;
-INSERT INTO contacts (name, email) VALUES ('$name', '$email');
-SET @parent_id = LAST_INSERT_ID();
-INSERT INTO contacts_details (contacts_ID, json) VALUES (@parent_id, '$json');
-INSERT INTO contacts_activity (contacts_ID, campaign_ID, activity_type, activity_date) VALUES $result;
+$batch_data
 COMMIT;
 EOF
 
+
+        end_time=$(date +%s%3N)
+        execution_time=$((end_time - start_time))
+
+        echo "batch execution time: $execution_time milliseconds"
+        j=0
+        batch_data=""
+    fi
+
 done
-end_time=$(date +%s%3N)  
-execution_time=$((end_time - start_time)) 
+
+end_time=$(date +%s%3N)
+execution_time=$((end_time - start_time))
 
 echo "Script execution time: $execution_time milliseconds"
