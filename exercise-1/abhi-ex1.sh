22ec5cb2f4f4704a4199b96200fc34c95deccebe
Abhinandh AV <abhinandh.avh@gmail.com>
2023-08-22 14:38:16 +0530

    sql query is added

--- exercise-1/abhi-ex1.sh
+++ exercise-1/abhi-ex1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 start_time=$(date +%s%3N)
-batch_size=10
+batch_size=10000
 
 generate_data() {
     data=""
@@ -50,25 +50,26 @@ generate_data() {
 
 
 count=1000000
-j=0  
+j=0
+  
 
-for ((i=1; i<=$count; i++)); do
+for ((i=21; i<=$count; i++)); do
    
-    if [[ $j -lt 1000 ]]; then  
-        name="Name$i"
-        email="name$i@example.com"
-        dob="01/01/2000"
-        country="Country$i"
-        city="City$i"
-        json="{\"dob\":\"$dob\",\"country\":\"$country\",\"city\":\"$city\"}"
-        result=$(generate_data)
+      
+    name="Name$i"
+    email="name$i@example.com"
+    dob="01/01/2000"
+    country="Country$i"
+    city="City$i"
+    json="{\"dob\":\"$dob\",\"country\":\"$country\",\"city\":\"$city\"}"
+    result=$(generate_data)
 
-        batch_data+="INSERT INTO contacts (name, email) VALUES ('$name', '$email');"
-        batch_data+="SET @parent_id = LAST_INSERT_ID();"
-        batch_data+="INSERT INTO contacts_details (contacts_ID, json) VALUES (@parent_id, '$json');"
-        batch_data+="INSERT INTO contacts_activity (contacts_ID, campaign_ID, activity_type, activity_date) VALUES $result;"
-        j=$((j+1))  
-    else
+    batch_data+="INSERT INTO contacts (name, email) VALUES ('$name', '$email');"
+    batch_data+="SET @parent_id = LAST_INSERT_ID();"
+    batch_data+="INSERT INTO contacts_details (contacts_ID, json) VALUES (@parent_id, '$json');"
+    batch_data+="INSERT INTO contacts_activity (contacts_ID, campaign_ID, activity_type, activity_date) VALUES $result;"
+    j=$((j+1))  
+    if [[ $j -gt $batch_size || $i -eq $count ]]; then
         mysql --defaults-file=~/.my.cnf -D DB <<EOF
 START TRANSACTION;
 $batch_data
@@ -90,3 +91,33 @@ end_time=$(date +%s%3N)
 execution_time=$((end_time - start_time))
 
 echo "Script execution time: $execution_time milliseconds"
+
+
+: <<COMMENT
+	# SQL query
+	query="
+	SELECT
+	    c.contacts_ID,
+	    a.name,
+	    a.email,
+	    JSON_UNQUOTE(JSON_EXTRACT(b.json, '$.country')) AS extracted_country,
+	    COUNT(activity_TYPE) AS total_ACTIVITY
+	FROM
+	    contacts_activity c
+	JOIN
+	    contacts a ON a.ID = c.contacts_ID
+	JOIN
+	    contacts_details b ON a.ID = b.contacts_ID
+	GROUP BY
+	    c.contacts_ID, a.name, b.json
+	ORDER BY
+	    COUNT(activity_TYPE) DESC
+	LIMIT 5;
+	"
+
+	# Execute the query using the mysql command
+	result=$(mysql --defaults-file=~/.my.cnf -D DB -Bse "$query")
+
+	# Print the result
+	echo "$result"
+COMMENT
