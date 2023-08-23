57a904515499474c2f23643722c3caf56a7c7d93
Dona Sussan Chacko <donasussan2000@gmail.com>
2023-08-23 12:35:27 +0530

    final code with 10000 contact/minute

--- exercise-1/dona-ex1.sh
+++ exercise-1/dona-ex1.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-DB_HOST="localhost"
-DB_USER="root"
-DB_NAME="campaign"
 
-contacts=1000000
+start_time=$(date +%s%3N)
 
+#array of date
 declare -A activity_dates
-
 activity_dates["1"]="2023-08-21"
 activity_dates["2"]="2023-08-22"
 activity_dates["3"]="2023-08-23"
@@ -15,105 +12,103 @@ activity_dates["5"]="2023-08-25"
 activity_dates["6"]="2023-08-26"
 activity_dates["7"]="2023-08-27"
 
+#get activity date
 function get_activity_date() {
-    activity_type="$1"
-    echo "${activity_dates[$activity_type]}"
+    echo "${activity_dates[$1]}"
 }
 
-
-function activitytype_fn() {
-random_number=$((1 + RANDOM % 8))
-
-case $random_number in
-    1)
-        values=(1 3 4 5)
-        ;;
-    2)
-        values=(1 3 4 6)
-        ;;
-    3)
-        values=(1 3 4 7)
-        ;;
-    4)
-        values=(2)
-        ;;
-    5)
-        values=(1)
-        ;;
-    6)
-        values=(1 3 6 7)
-        ;;
-    7)
-        values=(1 3 6)
-        ;;
-    8)
-        values=(1 3 7)
-        ;;
-    *)
-        values=("out of range")
-        ;;
-esac
-echo "${values[@]}"
-
+activity() {
+    local data=""
+    values=()
+    campaign=$((1 + RANDOM % 10))
+    random_activity=$((1 + RANDOM % 7))
+    activity_date="2023-07-01"
+    case $random_activity in
+        1) values=(1 3 4 5) ;;
+        2) values=(1 3 4 6) ;;
+        3) values=(1 3 4 7) ;;
+        4) values=(2) ;;
+        5) values=(1) ;;
+        6) values=(1 3 6 7) ;;
+        7) values=(1 3 6) ;;
+        8) values=(1 3 7) ;;
+        *) values=("out of range") ;;
+    esac
+    for array_value in "${values[@]}"; do
+        activity_date=$(get_activity_date "$array_value")
+        activity_string+=("(@last_id, $campaign, '$array_value', '$activity_date')")
+    done
+    
+    activity_values=$(IFS=,; echo "${activity_string[*]}")
+    echo "$activity_values"
 }
 
-
+#generate contact data
 function contact_data(){
     names=("Dona" "Joseph" "Deleesha" "Edison" "James" "Emma" "Michael" "Olivia" "William" "Ava" "David" "Sophia" "Joseph" "Isabella" "Charles" "Mia" "John" "Amelia" "Robert" "Grace" "Daniel" "Harper" "Matthew" "Sofia" "Andrew" "Scarlett" "Benjamin" "Abigail" "Samuel" "Emily" "Jackson" "Avery" "Christopher" "Chloe" "Joshua" "Ella" "David" "Aria" "John" "Lily" "Ethan" "Layla" "Ryan" "Zoey" "Alexander" "Grace" "James" "Aubrey" "Elijah" "Hannah" "Benjamin" "Ellie" "Samuel" "John" "Mary" "Michael" "Linda" "William" "Susan" "David" "Sarah" "Robert" "Karen" "James" "Patricia" "Richard" "Jennifer" "Charles" "Laura" "Joseph" "Nancy" "Thomas" "Maria")
     domains=("example" "testmail" "domain" "email" "mysite" "webmail" "company" "service" "provider" "online" "net" "org" "co" "shop" "store" "blog" "info" "business" "tech" "xyz")
-    countries=("UAE" "UK" "USA" "Uruguay" "Uzbekistan" "Vanuatu" "Vatican City" "Venezuela" "Vietnam" "Yemen" "Zambia" "Zimbabwe")
+    countries=("UAE" "UK" "USA" "Uruguay" "Uzbekistan" "Vanuatu" "Venezuela" "Vietnam" "Yemen" "Zambia" "Zimbabwe")
     campaign=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
-    
+
     _name=${names[$((RANDOM % ${#names[@]}))]}
     _domain=${domains[$((RANDOM % ${#domains[@]}))]}
-    _email="user${i}@${_domain}.com"
+    _email="user${1}@${_domain}.com"
     _dob=$(date -d "$((RANDOM % 36525)) days ago" +%Y-%m-%d)
     _country=${countries[$((RANDOM % ${#countries[@]}))]}
-    _city="${_country}${i}"
+    _city="${_country}${1}"
     _campaignid=${campaign[$((RANDOM % ${#campaign[@]}))]}
 
-    json="{\"DOB\": \"$_dob\", \"City\": \"$_city\", \"Country\": \"$_country\"}"
-
-    gen_name="$_name"
-    gen_email="$_email"
-    gen_json="$json"
-
-    #storing the last inserted id into result variable
-    result=$(mysql --defaults-file=~/.my.cnf -h "$DB_HOST" -D "$DB_NAME" -e "INSERT INTO Contacts (Name, Email)
-    VALUES ('$gen_name', '$gen_email'); SELECT LAST_INSERT_ID();")
-
-
-    id_value=${result##*)}
-    id=$((id_value))
-    #sending data into contact details table
-    mysql --defaults-file=~/.my.cnf -h "$DB_HOST" -D "$DB_NAME" -e "INSERT INTO ContactDetails(ContactsID,DataJSON)
-    VALUES ('$id','$gen_json');"
-
-
-    echo "$id"  # Return the 'result' variable
+    echo "$_name"
+    echo "$_email"
+    echo "$_dob"
+    echo "$_city"
+    echo "$_country"
     echo "$_campaignid"
-
 }
 
-for ((i = 0; i < contacts; i++)); do
-    id_and_campaign=($(contact_data))  # Call the function and capture the output in an array
-    id="${id_and_campaign[0]}"  # Extract 'id' from the array
-    campaign="${id_and_campaign[1]}" # Extract campaign id
-    activity_array=($(activitytype_fn))
-    
-    activity_values=()
-    for ((j = 0; j < "${#activity_array[@]}"; j++)); do
-        activity_type="${activity_array[j]}"
-        activity_date=$(get_activity_date "$activity_type")
-        activity_values+=("('$campaign', '$id', '$activity_type', '$activity_date')")
-    done
 
-   
-    _activity_values=$(IFS=,; echo "${activity_values[*]}")
+count=1000000
+batch=10000
+inc=0
+group=""
+batch_no=0
+
+for ((i=1; i<=$count; i++)); do
+        returned_data=($(contact_data "$i"))
+        gen_name="${returned_data[0]}"
+        gen_email="${returned_data[1]}"
+        gen_json="{\"DOB\": \"${returned_data[2]}\", \"City\": \"${returned_data[3]}\", \"Country\": \"${returned_data[4]}\"}"
+        campaign="${returned_data[5]}"
+        
+        result=$(activity)
+
+    group+="INSERT INTO Contacts (Name, Email) VALUES ('$gen_name', '$gen_email');"
+    group+="SET @last_id = LAST_INSERT_ID();"
+    group+="INSERT INTO ContactDetails (ContactsID, DataJSON) VALUES (@last_id, '$gen_json');"
+    group+="INSERT INTO ContactActivity (ContactsID, CampaignID, ActivityType, ActivityDate) VALUES $result;"
     
-    insert_query="INSERT INTO ContactActivity (CampaignID, ContactsID, ActivityType, ActivityDate) VALUES $_activity_values;"
-    
-    mysql --defaults-file=~/.my.cnf -h "$DB_HOST" -D "$DB_NAME" -e "$insert_query"
+    inc=$((inc+1))  
+
+    if [[ $inc -gt $batch || $i -eq $count ]]; then
+        mysql --defaults-file=~/.my.cnf -D campaign <<EOF
+START TRANSACTION;
+$group
+COMMIT;
+EOF
+batch_no=$((batch_no+1))  
+        final=$(date +%s%3N)
+        execution_time=$((final - start_time))
+
+        echo "batch $batch_no successful and execution time is: $execution_time milliseconds"
+        inc=0
+        group=""
+    fi
 done
 
-echo "successful"
+
+
+#SQL QUERY
+# SELECT co.ID, ca.CampaignID, co.Email, ca.ActivityType, JSON_EXTRACT(cd.DataJSON, '$.Country') 
+# FROM Contacts co JOIN ContactActivity ca ON co.ID = ca.ContactsID JOIN ContactDetails cd ON cd.ContactsID = co.ID 
+# WHERE ca.ActivityType = 3 AND JSON_EXTRACT(cd.DataJSON, '$.Country') in ('USA','UK') 
+# GROUP BY co.ID, cd.DataJSON, ca.CampaignID HAVING COUNT(*)>30; 
